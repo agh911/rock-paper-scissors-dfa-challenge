ff6f93bf355bc5bfc776a6cc634af26372549845
Andreea Ghenea <andreeag@Andreeas-MBP.lan>
2023-08-07 21:31:19 +0100

    Added game styling

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const port = 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('css'));
+app.use(express.static('assets'));
 
 import indexRouter from './routes/index.js';
 import gameRouter from './routes/game.js';

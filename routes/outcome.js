628a88f51e193cc9d04b91cf1298787bf0272d5e
Andreea Ghenea <andreeag@Andreeas-MBP.lan>
2023-08-08 17:19:41 +0100

    Implemented changes to be able to play a match of 3 rounds

--- routes/outcome.js
+++ routes/outcome.js
@@ -2,35 +2,26 @@ import express from 'express';
 
 const router = express.Router();
 
-function declareWinner(playerOption, computerOption) {
-    const winningCombinations = {
-        rock: 'scissors',
-        paper: 'rock',
-        scissors: 'paper'
-    }
-
-    if (playerOption === computerOption) {
-        return "It's a tie!";
-    }
-
-    if (winningCombinations[playerOption] === computerOption) {
-        return "You win!";
-    }
-
-    return "Computer wins!"
-}
-
 router.post('/', (req, res) => {
     const match = req.app.locals.match;
     const playerOption = req.body.option;
-    const computerOption = match.computer.chooseOption();
 
-    const result = declareWinner(playerOption, computerOption);
+    const result = match.playRound(playerOption);
+    match.roundsPlayed++;
 
+    let matchResult = result;
+
+    if (match.roundsPlayed === 3) {
+        matchResult = match.getMatchWinner();
+        res.render('matchResult', {
+            result: matchResult
+        })
+        match.reset();
+    }
     res.render('outcome', {
         playerOption: playerOption,
-        computerOption: computerOption,
-        result: result
+        computerOption: match.computer.chooseOption(),
+        result: matchResult,
     })
 })
 
--- src/match.js
+++ src/match.js
@@ -2,9 +2,55 @@ import Player from "./player.js";
 import ComputerPlayer from "./computerPlayer.js";
 
 class Match {
+    constructor() {
+        this.roundsPlayed = 0;
+        this.playerWins = 0;
+        this.computerWins = 0;
+    }
+
     setup(playerName) {
         this.player = new Player(playerName);
         this.computer = new ComputerPlayer();
+    }
+
+    playRound(playerOption) {
+        const computerOption = this.computer.chooseOption();
+
+        const winningCombinations = {
+            rock: 'scissors',
+            paper: 'rock',
+            scissors: 'paper'
+        }
+
+        if (playerOption === computerOption) {
+            return "It's a tie!";
+        }
+
+        if (winningCombinations[playerOption] === computerOption) {
+            this.playerWins++;
+            return "You win this round!";
+        }
+
+        this.computerWins++;
+        return "Computer wins this round!";
+    }
+
+    getMatchWinner() {
+        if (this.playerWins === this.computerWins) {
+            return "It's a tie!";
+        }
+
+        if (this.playerWins > this.computerWins) {
+            return `Congratulations, ${this.player.name} wins the match!`;
+        }
+
+        return "Computer wins the match!";
+    }
+
+    reset() {
+        this.roundsPlayed = 0;
+        this.playerWins = 0;
+        this.computerWins = 0;
     }
 }
 
